a5a887f8d7f65bf9862711638015c4f1e1a81831
Rohan Venkatapuram <sharpedo75@gmail.com>
2024-01-31 05:21:33 -0800

    Teleportation works

--- asgn1b/src/World/World.js
+++ asgn1b/src/World/World.js
@@ -10,7 +10,7 @@ import { Loop } from './systems/Loop.js';
 import { getDrawingMaterial, getGrassMaterial, getSkyboxMaterial } from './components/materials.js';
 import { createSphere } from './components/sphere.js';
 import { createTree } from './components/tree.js';
-import { Vector3, CanvasTexture, MeshBasicMaterial, Object3D, BufferGeometry, Line } from '../../lib/three.module.js';
+import { Vector3, CanvasTexture, MeshBasicMaterial, Object3D, BufferGeometry, Line, CircleGeometry, Mesh, Matrix4, Raycaster, RingGeometry, LineBasicMaterial, Float32BufferAttribute, AdditiveBlending, Quaternion } from '../../lib/three.module.js';
 import { createRock } from './components/rock.js';
 
 import { FogExp2 } from '../../lib/three.module.js';
@@ -25,12 +25,24 @@ let renderer;
 let scene;
 let loop;
 let hand1, hand2;
+
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 const handModels = {
   left: null,
   right: null
 };
+
+let INTERSECTION;
+let marker;
+const tempMatrix = new Matrix4();
+let raycaster;
+
+let baseReferenceSpace;
+
+let ground;
+
+const heightOffset = -2.5;
 
 class World {
   constructor(container) {
@@ -39,7 +51,7 @@ class World {
     renderer = createRenderer();
     scene = createScene();
 
-    loop = new Loop(camera, scene, renderer);
+    loop = new Loop(camera, scene, renderer, this.render);
     container.append(renderer.domElement);
 
     // Camera controls
@@ -87,7 +99,7 @@ class World {
 
     // Ground
     const grassMaterial = getGrassMaterial();
-    const ground = createBox(grassMaterial);
+    ground = createBox(grassMaterial);
     ground.position.y = -3;
     ground.scale.set(45, 1, 45);
     ground.receiveShadow = true;
@@ -171,7 +183,45 @@ class World {
   }
 
   render() {
-    renderer.render(scene, camera);
+    INTERSECTION = undefined;
+
+    if (controller1.userData.isSelecting === true) {
+
+      tempMatrix.identity().extractRotation(controller1.matrixWorld);
+
+      raycaster.ray.origin.setFromMatrixPosition(controller1.matrixWorld);
+      raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+
+      const intersects = raycaster.intersectObjects([ground]);
+
+      if (intersects.length > 0) {
+
+        INTERSECTION = intersects[0].point;
+
+      }
+
+    } else if (controller2.userData.isSelecting === true) {
+
+      tempMatrix.identity().extractRotation(controller2.matrixWorld);
+
+      raycaster.ray.origin.setFromMatrixPosition(controller2.matrixWorld);
+      raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+
+      const intersects = raycaster.intersectObjects([ground]);
+
+      if (intersects.length > 0) {
+
+        INTERSECTION = intersects[0].point;
+
+      }
+
+    }
+
+    if (INTERSECTION) {
+      marker.position.copy(INTERSECTION);
+    }
+
+    marker.visible = INTERSECTION !== undefined;
   }
 
   start() {
@@ -248,8 +298,93 @@ class World {
     });
   }
 
+  buildController(data) {
+
+    let geometry, material;
+
+    switch (data.targetRayMode) {
+
+      case 'tracked-pointer':
+
+        geometry = new BufferGeometry();
+        geometry.setAttribute('position', new Float32BufferAttribute([0, 0, 0, 0, 0, - 1], 3));
+        geometry.setAttribute('color', new Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3));
+
+        material = new LineBasicMaterial({ vertexColors: true, blending: AdditiveBlending });
+
+        return new Line(geometry, material);
+
+      case 'gaze':
+
+        geometry = new RingGeometry(0.02, 0.04, 32).translate(0, 0, - 1);
+        material = new MeshBasicMaterial({ opacity: 0.5, transparent: true });
+        return new Mesh(geometry, material);
+
+    }
+
+  }
+
+  setupControllerEvents() {
+    const buildController = this.buildController;
+
+    function onSelectStart() {
+      this.userData.isSelecting = true;
+
+    }
+
+    function onSelectEnd() {
+
+      this.userData.isSelecting = false;
+      console.log("AAAA");
+
+      if (INTERSECTION) {
+
+        const offsetPosition = { x: - INTERSECTION.x, y: - INTERSECTION.y + heightOffset, z: - INTERSECTION.z, w: 1 };
+        const offsetRotation = new Quaternion();
+        const transform = new XRRigidTransform(offsetPosition, offsetRotation);
+        const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace(transform);
+
+        renderer.xr.setReferenceSpace(teleportSpaceOffset);
+
+      }
+
+    }
+
+    controller1 = renderer.xr.getController(0);
+    controller1.addEventListener('selectstart', onSelectStart);
+    controller1.addEventListener('selectend', onSelectEnd);
+    controller1.addEventListener('connected', function (event) {
+
+      this.add(buildController(event.data));
+
+    });
+    controller1.addEventListener('disconnected', function () {
+
+      this.remove(this.children[0]);
+
+    });
+    scene.add(controller1);
+
+    controller2 = renderer.xr.getController(1);
+    controller2.addEventListener('selectstart', onSelectStart);
+    controller2.addEventListener('selectend', onSelectEnd);
+    controller2.addEventListener('connected', function (event) {
+
+      this.add(buildController(event.data));
+
+    });
+    controller2.addEventListener('disconnected', function () {
+
+      this.remove(this.children[0]);
+
+    });
+    scene.add(controller2);
+
+  }
+
   setupVR() {
     // Setup webxr button
+    renderer.xr.addEventListener('sessionstart', () => baseReferenceSpace = renderer.xr.getReferenceSpace());
     renderer.xr.enabled = true;
     const enterVRButton = document.getElementById("asgn5").appendChild(VRButton.createButton(renderer));
     enterVRButton.style.backgroundColor = "#04E762";
@@ -264,7 +399,7 @@ class World {
 
     // Setup dolly for moving the renderer
     const dolly = new Object3D();
-    dolly.position.z = 5;
+    // dolly.position.z = 5;
     dolly.add(camera);
     dolly.add(controller1);
     dolly.add(controller2);
@@ -272,17 +407,20 @@ class World {
     // Setup Hands
     this.setupHands(controllerModelFactory, handModelFactory, dolly);
 
-    // Setup hand lines
-    const geometry = new BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(0, 0, - 1)]);
+    // Setup Teleport Marker
+    marker = new Mesh(
+      new CircleGeometry(0.25, 32).rotateX(- Math.PI / 2),
+      new MeshBasicMaterial({ color: 0xbcbcbc })
+    );
+    scene.add(marker);
 
-    const line = new Line(geometry);
-    line.name = 'line';
-    line.scale.z = 5;
-
-    controller1.add(line.clone());
-    controller2.add(line.clone());
+    // Create raycaster for teleport location detection
+    raycaster = new Raycaster();
 
     scene.add(dolly);
+
+    // Setup Controller Events
+    this.setupControllerEvents();
 
     const dummyCam = new Object3D();
     camera.add(dummyCam);
